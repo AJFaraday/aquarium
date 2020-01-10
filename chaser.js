6ab18031a2241a007e311188c593a96e04926d22
andrew james faraday <ajfaraday@batman.alces.co.uk>
2020-01-10 13:09:24 +0000

    added chaser thing

--- chaser.js
+++ chaser.js
@@ -1,3 +1,35 @@
-Chaser = function()  {
+Chaser = function () {
+  this.target = new MoveTarget();
 
-} ;
+  this.x = (1024 / 2);
+  this.y = (768 / 2);
+  this.size = 10;
+
+  this.turn_speed = 20; // up to 100
+  this.speed = 20;
+  this.angle = 0;
+  this.history = [];
+
+  Object.assign(this, Follower);
+
+  this.update = function () {
+    this.move();
+  };
+
+  this.draw = function () {
+    Canvas.ctx.lineWidth = 5;
+    Canvas.ctx.fillStyle = 'rgba(256, 0,0, 0.5)';
+    Canvas.ctx.beginPath();
+
+    Canvas.ctx.arc(
+      this.x,
+      this.y,
+      this.size,
+      0,
+      360
+    );
+    Canvas.ctx.fill();
+  };
+
+
+};
