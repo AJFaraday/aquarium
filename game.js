1ddaf610be8f5862999144b266f3e1430bec2711
andrew james faraday <ajfaraday@batman.alces.co.uk>
2020-01-22 12:22:58 +0000

    mobile debug thing

--- game.js
+++ game.js
@@ -48,8 +48,12 @@ class Game {
   }
 
   static follow_touch(e) {
-    Player.set_target(e.originalEvent.touches[0].pageX, e.originalEvent.touches[0].pageY);
-    document.getElementById('debug').value = 'C - ' + JSON.stringify(e.originalEvent.touches[0].pageX);
+    try {
+      Player.set_target(e.originalEvent.touches[0].pageX, e.originalEvent.touches[0].pageY);
+      document.getElementById('debug').value = 'D - ' + JSON.stringify(e.originalEvent.touches[0].pageX);
+    } catch(error) {
+      document.getElementById('debug').value = 'D - ' + error);
+    }
   }
 
   static add_goals() {
