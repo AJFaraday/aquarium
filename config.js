cbc889520db7968d7d8800580a9a07cf74cfc6d3
andrew james faraday <ajfaraday@batman.alces.co.uk>
2020-02-06 16:47:25 +0000

    oops, missed one

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ Config = {
     {colour: 'rgb(0,255,0,0.4)', strategy: 'farthest'},
     {colour: 'rgb(0,0,255,0.4)', strategy: 'random'},
     {colour: 'rgb(128,128,128,0.4)', strategy: 'low_turning'},
+    {colour: 'rgb(128,0,128,0.4)', strategy: 'high_turning'},
     {colour: 'rgb(255,128,0,0.4)', strategy: 'top_hugger'},
     {colour: 'rgb(0,0,0,0.4)', strategy: 'parasite'}
   ]
