a18e82d6475a24e4303c0bcae6788a06d71580ed
andrew james faraday <ajfaraday@batman.alces.co.uk>
2020-03-04 17:04:52 +0000

    tinkering

--- static/ghost.js
+++ static/ghost.js
@@ -8,7 +8,7 @@ Static.Ghost = class Ghost {
     this.x = src.x;
     this.y = src.y;
     this.size = src.size;
-    this.colour = src.colour.replace(/0\.[0-9]/, '0.2');
+    this.colour = src.colour.replace(/0\.[0-9]/, '0.1');
     Game.drawables.push(this);
   }
 
