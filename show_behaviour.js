375ce22975e2326b8412b847f94fdcb8385f94a0
A Faraday (post baby) <ajfaraday@edge-ka.com>
2020-03-31 17:03:08 +0100

    started the snake specifif page

---
+++ show_behaviour.js
@@ -0,0 +1,78 @@
+class ShowBehaviour {
+
+  constructor(behaviour_name) {
+    this.behaviour_name = behaviour_name;
+    this.behaviour = new Behaviours[behaviour_name]();
+    this.name = this.behaviour.name();
+    document.getElementById('title').innerHTML = this.name;
+
+    var show = this;
+    this.matches = match_data.filter(
+      function (match) {
+        return Object.keys(match.scores).some(
+          function (score) {
+            return score == show.name;
+          }
+        );
+      }
+    );
+    this.matches = this.matches.sort(
+      function (match, other) {
+        var score_name = Object.keys(match.scores).filter(
+          function (score) {
+            return score == show.name;
+          }
+        )[0];
+        var value = match.scores[score_name].value;
+
+        var other_score_name = Object.keys(other.scores).filter(
+          function (score) {
+            return score == show.name;
+          }
+        )[0];
+        var other_value = other.scores[score_name].value;
+        return value - other_value;
+      }
+    );
+
+    this.build_table();
+  }
+
+  build_table() {
+    this.table = document.getElementById('match_table');
+
+    var table = document.createElement('table');
+
+    var thead = document.createElement('thead');
+    var tr = document.createElement('tr');
+    ['Match', 'Score', 'Position'].forEach(
+      function (header) {
+        var th = document.createElement('th');
+        th.innerHTML = header;
+        tr.appendChild(th);
+      }
+    );
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
+    var tbody = document.createElement('tbody');
+    this.matches.forEach(
+      function (match) {
+        var tr = document.createElement('tr');
+        [].forEach( // TODO these, all of these
+          function (cell) {
+            var td = document.createElement('td');
+            td.innerHTML = cell;
+            tr.appendChild(td);
+          }
+        );
+        tbody.appendChild(tr);
+      }
+    );
+    table.appendChild(tbody);
+
+    this.table.appendChild(table);
+  }
+
+
+}
